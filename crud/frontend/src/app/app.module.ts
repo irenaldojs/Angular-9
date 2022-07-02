98d4e7b0e238c98b457128ef67fcf4a2fb1fb3a3
Irenaldo Jr da Silva <irenaldojs@gmail.com>
2022-07-02 00:07:16 -0300

    11# Navegar para Tela de Criar Produtos

--- crud/frontend/src/app/app.module.ts
+++ crud/frontend/src/app/app.module.ts
@@ -11,8 +11,9 @@ import { NavComponent } from './components/template/nav/nav.component';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule } from '@angular/material/card';
-
+import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { RedDirective } from './directives/red.directive';
@@ -39,6 +40,7 @@ import { ProductCreateComponent } from './components/product/product-create/prod
     MatListModule,
     MatSidenavModule,
     MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
